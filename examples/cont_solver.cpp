25834a402d5602cf5b5828a2f4f792fdf3a53f89
Aykut Bulut <aykutblt@gmail.com>
2016-04-14 20:58:37 -0400

    mosek continuous solver example improved.

--- examples/cont_solver.cpp
+++ examples/cont_solver.cpp
@@ -4,14 +4,26 @@
 #include <OsiMosekSolverInterface.hpp>
 #include <iostream>
 #include <iomanip>
+#include <mosek.h>
+
+static void MSKAPI printstr(void *handle, MSKCONST char str[]) {
+  printf("%s",str);
+} /* printstr */
 
 int main(int argc, char ** argv) {
   OsiConicSolverInterface * solver = new OsiMosekSolverInterface();
   solver->readMps(argv[1]);
+  clock_t start_time = clock();
   solver->initialSolve();
-  std::cout << "Objective is " << std::setprecision(15)
-	    << solver->getObjValue() << std::endl;
+  clock_t duration = clock() - start_time;
   // write mps file
-  solver->writeMps("problem");
+  // solver->writeMps("problem");
+  MSKtask_t task = dynamic_cast<OsiMosekSolverInterface*>(solver)->getLpPtr();
+  MSK_linkfunctotaskstream(task,MSK_STREAM_LOG,NULL,printstr);
+  MSK_solutionsummary(task, MSK_STREAM_MSG);
+  std::cout << "Objective is  : " << std::setprecision(15)
+	    << solver->getObjValue() << std::endl;
+  std::cout << "CPU time spent: " << double(duration)/double(CLOCKS_PER_SEC)
+	    << std::endl;
   return 0;
 }
